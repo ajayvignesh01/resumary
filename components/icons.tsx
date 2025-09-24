62a81c0d4d62b1a5efe045396ad79ef272ecda9a
ajayvignesh01 <ajayvignesh01@gmail.com>
2025-09-24 12:07:03 -0400

    feat: icon

---
+++ components/icons.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+
+export const ResumaryIcon = ({}: ComponentProps<'svg'>) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width='36' height='44' viewBox='0 0 36 44' fill='none'>
+    <path
+      d='M20.5333 29.3333C28.6335 29.3333 35.2 22.7668 35.2 14.6667C35.2 6.56649 28.6335 0 20.5333 0H0V29.3333H20.5333Z'
+      fill='#F38744'
+    />
+    <path d='M0 0L35.2 44H11.7333L0 29.3333V0Z' fill='#2E90FA' />
+    <path
+      d='M23.2637 29.0796C22.379 29.2462 21.4664 29.3333 20.5333 29.3333H0V0L23.2637 29.0796Z'
+      fill='#53B1FD'
+    />
+  </svg>
+)
